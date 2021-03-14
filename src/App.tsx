19adc8e1db98474cad20b64795e7f0f3049213e4
Jake Billings <git@jakebillings.com>
2021-03-14 17:14:42 -0600

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
 import React from 'react';
 import './App.css';
-import {FirebaseDatabaseNode, FirebaseDatabaseProvider} from "@react-firebase/database";
+import {FirebaseDatabaseProvider} from "@react-firebase/database";
 import firebase from "firebase";
 import firebaseConfig from "./firebaseConfig";
-import {FirebaseDatabaseNodesState} from "@react-firebase/database/dist/components/FirebaseDatabaseNodes";
-import {Place} from "./types/Place";
-import PlaceComponent from "./components/PlaceComponent";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect
+} from "react-router-dom";
+import PlacesView from "./views/PlacesView";
 
 function App() {
     return (
         <FirebaseDatabaseProvider firebase={firebase} {...firebaseConfig}>
-            <FirebaseDatabaseNode
-                path="/places"
-                limitToFirst={5}
-                orderByValue={"name"}
-            >
-                {(d: FirebaseDatabaseNodesState) => (
-                    <>
-                        {!(!d.isLoading && d.value) && (
-                            <>
-                                <h1>Loading...</h1>
-                            </>
-                        )}
-                        {!d.isLoading && d.value && (d.value as unknown as [Place]).map((place, index) => (
-                            <div  key={index}>
-                                <PlaceComponent place={place}/>
-                            </div>
-                        ))}
-                    </>
-                )}
-            </FirebaseDatabaseNode>
+            <Router>
+                <Switch>
+                    <Route exact path="/places" component={PlacesView} />
+                    <Redirect to="/places" />
+                </Switch>
+            </Router>
         </FirebaseDatabaseProvider>
     );
 }
